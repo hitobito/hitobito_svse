f43a6cbeb46b7a4f3903c20e5dcec063b54ecf74
psunix <psunix@s-it.ch>
2021-06-08 15:23:37 +0200

    Fix people seeder

--- db/seeds/development/1_people.rb
+++ db/seeds/development/1_people.rb
@@ -39,5 +39,5 @@ seeder.seed_all_roles
 
 root = Group.root
 devs.each do |name, email|
-  seeder.seed_developer(name, email, root, Group::Root::Leader)
+  seeder.seed_developer(name, email, root, Group::Svse::Geschaeftsleitung)
 end
